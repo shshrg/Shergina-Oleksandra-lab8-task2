9bd6e261bb7136b525cdcd5c113b2229dd601166
Oleksandra Shergina <sasha.shergina@gmail.com>
2023-11-06 14:59:42 +0200

    added documentation

--- puzzle.py
+++ puzzle.py
@@ -1,3 +1,13 @@
 '''Puzzle game'''
 def validate_board(board:list) -> bool:
+    '''
+    Покликання на GitHub:
+    https://github.com/shshrg/Shergina-Oleksandra-lab8-task2.git
+
+    Checks if the board is ready to start the game.
+    >>> board = ["**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *",\
+"3   1  **", "  8  2***", "  2  ****"]
+    >>> validate_board(board)
+    False
+    '''
     pass
